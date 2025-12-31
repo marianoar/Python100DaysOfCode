41eb9f49e52637c8355ea6f34ec95b8af4605a15
Mariano Arias <mariano_arias@epamneoris.com>
2025-12-31 12:22:10 -0300

    01 intro

---
+++ 01/task.py
@@ -0,0 +1,22 @@
+# Write your code below this line 👇
+print("Hello Mariano")
+
+name = input("What is your name?")
+
+print(name.capitalize())
+
+print(len(name))
+name = input("What is your name again?")
+
+print(name)
+print(len(name))
+
+print(len(input("What is your name?")))
+
+## task day 1
+print("Welcome to the Band Name Generator")
+city = input("What the name of the city you grew up in? ")
+print(city)
+pet = input("What's your pet's name?")
+print(pet)
+print("Your band name could be "+city+" "+pet)
